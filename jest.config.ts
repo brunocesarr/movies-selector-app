84e4f5470246089616e2d442fd744231b1770005
brunocesarr <brunoc.15@hotmail.com>
2022-06-24 11:06:39 -0300

    #Setup: Altering transform modules jest.

--- jest.config.ts
+++ jest.config.ts
@@ -43,7 +43,7 @@ const config: Config.InitialOptions = {
   transform: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/setup-test/fileTransformer.js",
-    // "^.+\\.tsx?$": "ts-jest",
+    "\\.[jt]sx?$": "babel-jest",
   },
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)",
