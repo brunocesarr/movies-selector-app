e6f5433e52860693f466e68477c1a5093384b794
brunocesarr <brunoc.15@hotmail.com>
2022-06-25 02:04:53 -0300

    #Setup: Alter config to ts files in jest.

--- jest.config.ts
+++ jest.config.ts
@@ -44,6 +44,7 @@ const config: Config.InitialOptions = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/setup-test/fileTransformer.js",
     "\\.[jt]sx?$": "babel-jest",
+    '^.+\\.ts': 'ts-jest',
   },
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)",
